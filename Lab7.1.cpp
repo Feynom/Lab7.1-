ef51d4cb425507fb1c4e07ebcea9787f823db497
Feynom <53947611+Feynom@users.noreply.github.com>
2021-05-11 11:48:00 +0300

    Add files via upload

---
+++ Lab7.1.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <array>
+
+using namespace std;
+
+void Create(array<int, 100>& arr, int n);
+void Print(array<int, 100>& arr, int n);
+int IndexOfMinOddElement(array<int, 100>& arr, int n);
+
+int main()
+{
+    srand((unsigned int)time(NULL));
+
+    int n;
+    cout << "Enter a size of array: "; cin >> n;
+
+    array<int, 100> arr = { 0 };
+
+    Create(arr, n);
+    Print(arr, n);
+    cout << "Index of min odd element in array: " << IndexOfMinOddElement(arr, n) << endl;
+
+    return 0;
+}
+
+void Create(array<int, 100>& arr, int n)
+{
+    for (int i = 0; i < n; i++)
+        arr[i] = rand() % 100;
+}
+void Print(array<int, 100> &arr, int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+int IndexOfMinOddElement(array<int, 100>& arr, int n)
+{
+    double minOddElem;
+    unsigned int minOddElemIndex = -1; //If there is no min odd element
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] % 2 != 0)
+        {
+            minOddElem = arr[i];
+            minOddElemIndex = i;
+            break;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] % 2 != 0 && arr[i] < minOddElem)
+        {
+            minOddElem = arr[i];
+            minOddElemIndex = i;
+        }
+    }
+
+    return minOddElemIndex;
+}
---
+++ UnitTest7.1.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab7.1/Lab7.1.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest71
+{
+	TEST_CLASS(UnitTest71)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			array<int, 100> arr = {0, 1, 2, 5, 7};
+			Assert::AreEqual(IndexOfMinOddElement(arr, 5), 1);
+
+			array<int, 100> arr2 = { 0, 0, 0, 6, 7 };
+			Assert::AreEqual(IndexOfMinOddElement(arr2, 5), 4);
+			
+			array<int, 100> arr3 = { 0, 0, 0, 0, 0 };
+			Assert::AreEqual(IndexOfMinOddElement(arr3, 5), -1);
+
+			array<int, 100> arr4 = { 0, 12, 20, 50, 70 };
+			Assert::AreEqual(IndexOfMinOddElement(arr4, 5), -1);
+
+			array<int, 100> arr5 = { 3, 7, 9, 13, 15 };
+			Assert::AreEqual(IndexOfMinOddElement(arr5, 5), 0);
+		}
+	};
+}
